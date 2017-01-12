614f8d908d39d0e8678f14144b06a03c59f44933
gnczoli <gnczoli@gmail.com>
2017-01-12 20:40:34 +0100

    Local changes

--- app/src/main/java/gz/rmbgysz/ahitat/MainAppActivity.java
+++ app/src/main/java/gz/rmbgysz/ahitat/MainAppActivity.java
@@ -87,8 +87,6 @@ public class MainAppActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_slideshow) {
 
-        //} else if (id == R.id.nav_manage) {
-
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
