01fafc6ba9534e266e8e8cb5e8404bb64d7fe3bc
ggza <geza.gonczi@gmail.com>
2017-04-23 15:56:36 +0300

    Ujj nagyítva egy picit, infó oldal hozzáadva

--- app/src/main/java/gz/rmbgysz/ahitat/MainActivity.java
+++ app/src/main/java/gz/rmbgysz/ahitat/MainActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDelegate;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
 
 import java.io.IOException;
 
@@ -24,6 +28,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startMainApp(View view) {
+        try {
+            DatabaseHelper.getInstance(this).createDataBase();
+            Intent intent = new Intent(this, MainAppActivity.class);
+            startActivity(intent);
+        } catch (IOException ioe) {
+            throw new Error("Unable to create database");
+        }
+    }
+
+    ImageView rotateImage;
+
+    public void startRotatingImage(View view) {
+        rotateImage = (ImageView) findViewById(R.id.rotate_image);
+        Animation startRotateAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.android_rotate_animation);
+        rotateImage.startAnimation(startRotateAnimation);
         try {
             DatabaseHelper.getInstance(this).createDataBase();
             Intent intent = new Intent(this, MainAppActivity.class);
