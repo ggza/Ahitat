0d7f5fb7c70eb5ea87c98810885c1c5051ada91e
Geza Gonczi <geza.gonczi@gmail.com>
2017-12-18 21:10:17 +0200

    Infó meg help oldal simítások

--- app/src/main/java/gz/rmbgysz/ahitat/AhitatIconIcons.java
+++ app/src/main/java/gz/rmbgysz/ahitat/AhitatIconIcons.java
@@ -1,10 +1,23 @@
 package gz.rmbgysz.ahitat;
 
-import com.joanzapata.iconify.Icon;
+import gz.rmbgysz.ahitat.Icon;
 
 public enum AhitatIconIcons implements Icon {
-    ahitat_book('\ue900'),
-    ahitat_heart('\ue87d');
+    ahitat_book('\ue906'),
+    ahitat_heart('\ue87d'),
+	ahitat_searchbydate('\ue916'),
+	ahitat_calendar('\ue24f'),
+	ahitat_favourite('\ue838'),
+	ahitat_help('\ue887'),
+	ahitat_information('\ue88e'),
+	ahitat_ujj('\ue900'),
+	ahitat_locallibrary('\ue54b'),
+	ahitat_actionname('\ue8b0'),
+	ahitat_myshare('\ue80d'),
+	ahitat_touchapp('\ue913'),
+	ahitat_menu('\ue5d2'),
+    ahitat_bookopenvariant('\ue901'),
+	ahitat_search('\ue8fa');
     
     char character;
 
--- app/src/main/java/gz/rmbgysz/ahitat/HelpActivity.java
+++ app/src/main/java/gz/rmbgysz/ahitat/HelpActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.graphics.Typeface;
 import android.widget.TextView;
-import com.joanzapata.iconify.Iconify;
 
 public class HelpActivity extends AppCompatActivity {
     TextView help_content1, help_title1, help_content6, help_title2, help_content2, help_content3, help_content4, help_content5, help_content7;
@@ -60,7 +59,6 @@ public class HelpActivity extends AppCompatActivity {
             }
         });
         */
-
     }
 
     @Override
