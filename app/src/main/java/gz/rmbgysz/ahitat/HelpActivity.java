8d83c6153e96be18d2da16c04ab53853585781b8
gzoli <gnczoli@gmail.com>
2017-12-20 17:31:59 +0100

    linkify

--- app/src/main/java/gz/rmbgysz/ahitat/HelpActivity.java
+++ app/src/main/java/gz/rmbgysz/ahitat/HelpActivity.java
@@ -3,9 +3,15 @@ package gz.rmbgysz.ahitat;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
 import android.view.MenuItem;
 import android.graphics.Typeface;
 import android.widget.TextView;
+
+import static android.text.Html.fromHtml;
 
 public class HelpActivity extends AppCompatActivity {
     TextView  help_title1, help_title2, help_title3, help_title4, help_content1, help_content2, help_content3, help_content4, help_content5, help_content6, help_content7;
@@ -26,17 +32,17 @@ public class HelpActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.help_toolbar);
         setSupportActionBar(toolbar);
 
-        help_title1=(TextView)findViewById(R.id.help_title1);
-        help_title2=(TextView)findViewById(R.id.help_title2);
-        help_title3=(TextView)findViewById(R.id.help_title3);
-        help_title4=(TextView)findViewById(R.id.help_title4);
-        help_content1=(TextView)findViewById(R.id.help_content1);
-        help_content2=(TextView)findViewById(R.id.help_content2);
-        help_content3=(TextView)findViewById(R.id.help_content3);
-        help_content4=(TextView)findViewById(R.id.help_content4);
-        help_content5=(TextView)findViewById(R.id.help_content5);
-        help_content6=(TextView)findViewById(R.id.help_content6);
-        help_content7=(TextView)findViewById(R.id.help_content7);
+        TextView help_title1 = (TextView)findViewById(R.id.help_title1);
+        TextView help_title2 = (TextView)findViewById(R.id.help_title2);
+        TextView help_title3 = (TextView)findViewById(R.id.help_title3);
+        TextView help_title4 = (TextView)findViewById(R.id.help_title4);
+        TextView help_content1 = (TextView)findViewById(R.id.help_content1);
+        TextView help_content2 = (TextView)findViewById(R.id.help_content2);
+        TextView help_content3 = (TextView)findViewById(R.id.help_content3);
+        TextView help_content4 = (TextView)findViewById(R.id.help_content4);
+        TextView help_content5 = (TextView)findViewById(R.id.help_content5);
+        TextView help_content6_tv = (TextView)findViewById(R.id.help_content6);
+        TextView help_content7 = (TextView)findViewById(R.id.help_content7);
 
         help_title1.setTypeface(Typeface.createFromAsset(getAssets(), M_BOLD));
         help_title2.setTypeface(Typeface.createFromAsset(getAssets(), M_BOLD));
@@ -47,13 +53,20 @@ public class HelpActivity extends AppCompatActivity {
         help_content3.setTypeface(Typeface.createFromAsset(getAssets(), M_REGULAR));
         help_content4.setTypeface(Typeface.createFromAsset(getAssets(), M_REGULAR));
         help_content5.setTypeface(Typeface.createFromAsset(getAssets(), M_REGULAR));
-        help_content6.setTypeface(Typeface.createFromAsset(getAssets(), M_REGULAR));
+        help_content6_tv.setTypeface(Typeface.createFromAsset(getAssets(), M_REGULAR));
         help_content7.setTypeface(Typeface.createFromAsset(getAssets(), M_REGULAR));
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
+
+        //help_content6_tv.setText(Html.fromHtml(getString(R.string.help_content6)));
+        //help_content6_tv.setMovementMethod(LinkMovementMethod.getInstance());
+
+        Linkify.addLinks(help_content6_tv, Linkify.EMAIL_ADDRESSES);
+
+        //help_content6_tv.setAutoLinkMask(Linkify.EMAIL_ADDRESSES);
 
         /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
