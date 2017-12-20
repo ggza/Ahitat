f9f2f66c62992c51d677092cf7319f9931467ef6
ggza <geza.gonczi@gmail.com>
2017-12-20 06:35:38 +0200

    Javítás a helpben...

--- app/src/main/java/gz/rmbgysz/ahitat/HelpActivity.java
+++ app/src/main/java/gz/rmbgysz/ahitat/HelpActivity.java
@@ -8,7 +8,7 @@ import android.graphics.Typeface;
 import android.widget.TextView;
 
 public class HelpActivity extends AppCompatActivity {
-    TextView help_content1, help_title1, help_content6, help_title2, help_content2, help_content3, help_content4, help_content5, help_content7;
+    TextView  help_title1, help_title2, help_title3, help_content1, help_content2, help_content3, help_content4, help_content5, help_content6, help_content7;
 
     private String M_BOLD= "merriweather_bold.ttf";
     private String M_REGULAR= "merriweather_regular.ttf";
@@ -28,20 +28,24 @@ public class HelpActivity extends AppCompatActivity {
 
         help_title1=(TextView)findViewById(R.id.help_title1);
         help_title2=(TextView)findViewById(R.id.help_title2);
+        help_title3=(TextView)findViewById(R.id.help_title3);
         help_content1=(TextView)findViewById(R.id.help_content1);
         help_content2=(TextView)findViewById(R.id.help_content2);
         help_content3=(TextView)findViewById(R.id.help_content3);
         help_content4=(TextView)findViewById(R.id.help_content4);
         help_content5=(TextView)findViewById(R.id.help_content5);
+        help_content6=(TextView)findViewById(R.id.help_content6);
         help_content7=(TextView)findViewById(R.id.help_content7);
 
         help_title1.setTypeface(Typeface.createFromAsset(getAssets(), M_BOLD));
         help_title2.setTypeface(Typeface.createFromAsset(getAssets(), M_BOLD));
+        help_title3.setTypeface(Typeface.createFromAsset(getAssets(), M_BOLD));
         help_content1.setTypeface(Typeface.createFromAsset(getAssets(), M_REGULAR));
         help_content2.setTypeface(Typeface.createFromAsset(getAssets(), M_REGULAR));
         help_content3.setTypeface(Typeface.createFromAsset(getAssets(), M_REGULAR));
         help_content4.setTypeface(Typeface.createFromAsset(getAssets(), M_REGULAR));
         help_content5.setTypeface(Typeface.createFromAsset(getAssets(), M_REGULAR));
+        help_content6.setTypeface(Typeface.createFromAsset(getAssets(), M_REGULAR));
         help_content7.setTypeface(Typeface.createFromAsset(getAssets(), M_REGULAR));
 
         if (getSupportActionBar() != null) {
